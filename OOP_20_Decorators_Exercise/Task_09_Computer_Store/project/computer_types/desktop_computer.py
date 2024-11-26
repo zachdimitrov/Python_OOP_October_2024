f6da619ba306525a84a4aa45cda00492fa57b084
Zahari Dimitrov <zach_dimitrov@yahoo.com>
2024-11-27 01:38:39 +0200

    change to file

--- OOP_20_Decorators_Exercise/Task_09_Computer_Store/project/computer_types/desktop_computer.py
+++ OOP_20_Decorators_Exercise/Task_09_Computer_Store/project/computer_types/desktop_computer.py
@@ -4,5 +4,3 @@ from project.computer_types.computer import Computer
 class DesktopComputer(Computer):
     def __init__(self, manufacturer: str, model: str):
         super().__init__(manufacturer, model)
-
-    
